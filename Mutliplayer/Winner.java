72e24483efba42f7af1dbcb3879fa6e3da1b094f
Devan F <116249842+devanfer02@users.noreply.github.com>
2022-11-22 20:06:02 +0700

    Update Winner.java

--- Mutliplayer/Winner.java
+++ Mutliplayer/Winner.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 
 public class Winner extends BasicAdder{
     public JPanel winPanel = new JPanel();
+    public JLabel congrats = new JLabel("Congratulations...",SwingConstants.CENTER);
     public JLabel won = new JLabel();
     public JButton newGameBtn = new JButton("NEW GAME");
     public JButton menuBtn = new JButton("MENU");
@@ -14,12 +15,14 @@ public class Winner extends BasicAdder{
         winPanel.setLayout(layout);
         setSpace(winPanel);
 
-
+        setFont(congrats,20);
         setFont(won, 55);
         setBtnColor(newGameBtn);
         setBtnColor(menuBtn);
-        addComponent(0,0,5,won,winPanel);
-        addComponent(0,1,5,newGameBtn,winPanel);
-        addComponent(0,2,5,menuBtn,winPanel);
+
+        addComponent(0,0,5,congrats,winPanel);
+        addComponent(0,1,5,won,winPanel);
+        addComponent(0,2,5,newGameBtn,winPanel);
+        addComponent(0,3,5,menuBtn,winPanel);
     }
 }
