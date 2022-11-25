b39e251e40594ee6ba8816ce11ad84cac6b4c61a
Devan F <116249842+devanfer02@users.noreply.github.com>
2022-11-25 19:14:52 +0700

    Update DuelGame.java

--- Singleplayer/DuelGame.java
+++ Singleplayer/DuelGame.java
@@ -7,36 +7,36 @@ public class DuelGame extends BasicAdder {
     public JPanel duelGamePanel = new JPanel();
     public JLabel Player = new JLabel("PLAYER",SwingConstants.CENTER);
     public JLabel Enemy = new JLabel("ENEMY",SwingConstants.CENTER);
-    public JLabel PlayerHP = new JLabel();
-    public JLabel PlayerAtk = new JLabel();
-    public JLabel EnemyHP = new JLabel();
-    public JLabel EnemyAtk = new JLabel();
-    public JLabel duelStatus1 = new JLabel(" \t ",SwingConstants.CENTER);
+    public JLabel PlayerHP = new JLabel("HITPOINT : ");
+    public JLabel PlayerAtk = new JLabel("ATK RANGE : ");
+    public JLabel EnemyHP = new JLabel("HITPOINT : ");
+    public JLabel EnemyAtk = new JLabel("ATK RANGE : ");
+    public JLabel duelStatus1 = new JLabel(" ",SwingConstants.CENTER);
     public JLabel duelStatus2 = new JLabel("WAITING FOR THE ROLL...",SwingConstants.CENTER);
     public JButton rollDuel = new JButton("ROLL TURN");
-    public JButton attack = new JButton("ATTACK");
-    public JButton heal = new JButton("HEAL");
-    public JButton poison = new JButton("POISON");
-    public JButton surrender = new JButton("SURRENDER");
+    public JButton attackBtn = new JButton("ATTACK");
+    public JButton healBtn = new JButton("HEAL");
+    public JButton poisonBtn = new JButton("POISON");
+    public JButton surrenderBtn = new JButton("SURRENDER");
     public JButton settingsDuel = new JButton("SETTINGS");
 
     public DuelGame(){
         duelGamePanel.setLayout(layout);
         setSpace(duelGamePanel);
 
-        setFont(Player,22);
-        setFont(Enemy,22);
-        setFont(PlayerHP,22);
-        setFont(PlayerAtk,22);
-        setFont(EnemyHP,22);
-        setFont(EnemyAtk,22);
-        setFont(duelStatus1,22);
-        setFont(duelStatus2,22);
+        setFont(Player,26);
+        setFont(Enemy,26);
+        setFont(PlayerHP,26);
+        setFont(PlayerAtk,26);
+        setFont(EnemyHP,26);
+        setFont(EnemyAtk,26);
+        setFont(duelStatus1,26);
+        setFont(duelStatus2,26);
         setBtnColor(rollDuel);
-        setBtnColor(attack);
-        setBtnColor(heal);
-        setBtnColor(poison);
-        setBtnColor(surrender);
+        setBtnColor(attackBtn);
+        setBtnColor(healBtn);
+        setBtnColor(poisonBtn);
+        setBtnColor(surrenderBtn);
         setBtnColor(settingsDuel);
 
         addComponent(0,0,1,Player,duelGamePanel);
