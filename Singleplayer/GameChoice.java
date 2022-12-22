80a42d0c8a51a73d1816f0afc0f0e55354faea9b
Devan F <116249842+devanfer02@users.noreply.github.com>
2022-12-23 02:57:42 +0700

    Update GameChoice.java

--- Singleplayer/GameChoice.java
+++ Singleplayer/GameChoice.java
@@ -8,7 +8,6 @@ public class GameChoice extends BasicAdder {
     public JLabel titleChoice = new JLabel("GAME CHOICE");
     public JButton storyBtn = new JButton("STORY MODE");
     public JButton duelBtn = new JButton("DUEL MODE");
-    public JButton survivalBtn = new JButton("SURVIVAL MODE");
     public JButton menuBtn = new JButton("MENU");
 
     public GameChoice(){
@@ -18,13 +17,11 @@ public class GameChoice extends BasicAdder {
         setFont(titleChoice, 40);
         setBtnColor(storyBtn);
         setBtnColor(duelBtn);
-        setBtnColor(survivalBtn);
         setBtnColor(menuBtn);
 
         addComponent(0,0,3,titleChoice,choicePanel);
         addComponent(0,1,3,storyBtn,choicePanel);
         addComponent(0,2,3,duelBtn,choicePanel);
-        addComponent(0,3,3,survivalBtn,choicePanel);
         addComponent(0,4,3,menuBtn,choicePanel);
     }
 }
